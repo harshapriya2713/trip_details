92f2d5cd6ea41243790c8f78273b12236b28da89
Harsha priya <82520712+harshapriya2713@users.noreply.github.com>
2023-11-30 17:15:48 +0530

    Add files via upload

--- tripform.sql
+++ tripform.sql
@@ -1,18 +1,29 @@
 CREATE TABLE trip_details (
     id SERIAL PRIMARY KEY,
-    Full_Name VARCHAR(255),
-    Phone_Number VARCHAR(15),
-    Email VARCHAR(255),
-    Destination VARCHAR(255),
-    Mode_Of_Travel VARCHAR(50),
-    Count_Of_People INT,
-    StartDate DATE,
-    End_Date DATE,
-    Purpose_Of_Trip VARCHAR(50),
-    Mention_Other_Facilities TEXT
+    full_name VARCHAR(255),
+    phone_number VARCHAR(15),
+    email VARCHAR(255),
+    destination VARCHAR(255),
+    mode_of_travel VARCHAR(50),
+    count_of_people INT,
+    startdate DATE,
+    end_date DATE,
+    purpose_of_trip VARCHAR(50),
+    mention_other_facilities TEXT
 );
 select * from trip_details;
 
 select current_user;
 drop table trip_details;
 
+CREATE TABLE trip_people (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255) 
+    person_name VARCHAR(255)
+);
+
+CREATE TABLE trip_purpose (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(255)
+    purpose_description TEXT 
+);
